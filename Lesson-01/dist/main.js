673b8c3676f9f580f5b6c3209010dc361e8f32e0
Syed Saquib Asghar <saq9516@gmail.com>
2023-10-19 18:49:07 +0530

    create the lesson 1

--- Lesson-01/dist/main.js
+++ Lesson-01/dist/main.js
@@ -1,2 +1,11 @@
 "use strict";
 let a = 90;
+let b = "6";
+let c = '';
+let userName = "Saquib";
+console.log(`My name is ${userName}`);
+console.log(a);
+c = a + b;
+console.log(c); // --> 906 (type: string)
+// c = (a / b) /* The right-hand side of an arithmetic operation must be of type 'any', 'number', 'bigint' or an enum type. */
+// console.log(c) 
--- Lesson-01/src/main.ts
+++ Lesson-01/src/main.ts
@@ -1,1 +1,18 @@
-let a:number = 90;
+let a: number = 90;
+let b: string = "6";
+let c: string = '';
+
+let userName: string = "Saquib";
+
+console.log(`My name is ${userName}`)
+
+console.log(a);
+
+c = a + b;
+console.log(c)  // --> 906 (type: string)
+
+// c = (a / b) /* The right-hand side of an arithmetic operation must be of type 'any', 'number', 'bigint' or an enum type. */
+
+// console.log(c) 
+
+

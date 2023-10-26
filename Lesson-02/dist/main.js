e3a73ed8e4a6e7719e239f8c1594a0b4be088ae3
Syed Saquib Asghar <saq9516@gmail.com>
2023-10-26 20:14:56 +0530

    Create lesson 2

---
+++ Lesson-02/dist/main.js
@@ -0,0 +1,20 @@
+"use strict";
+let userName = "Saquib";
+const num1 = 10;
+console.log(userName);
+console.log(num1);
+const anotherUserName = "Asghar";
+console.log(anotherUserName);
+const num2 = 234;
+console.log(num2);
+let isLoggedIn = true;
+console.log(`Is user Logged in : ${isLoggedIn}`);
+function greet() {
+    console.log('Good morning');
+}
+let anyVariable = 'Qwerty';
+console.log(anyVariable);
+anyVariable = 134;
+console.log(anyVariable);
+const sum = (a, b) => a + b;
+console.log(sum(13, 7));
---
+++ Lesson-02/src/main.ts
@@ -0,0 +1,34 @@
+let userName = "Saquib";
+const num1 = 10;
+console.log(userName);
+console.log(num1);
+
+const anotherUserName: string = "Asghar";
+console.log(anotherUserName);
+
+const num2: number = 234;
+console.log(num2);
+
+// userName = 29
+// anotherUserName = true
+
+let isLoggedIn: boolean = true;
+
+console.log(`Is user Logged in : ${isLoggedIn}`)
+
+function greet() {
+    console.log('Good morning')
+}
+
+let anyVariable: any = 'Qwerty';
+
+console.log(anyVariable);
+
+anyVariable = 134;
+console.log(anyVariable);
+
+const sum = (a: number, b: number): number => a + b;
+
+console.log(sum(13, 7))
+
+
